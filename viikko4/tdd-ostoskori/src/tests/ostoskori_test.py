660fbc344eeceef9ef69241a8c18cdc37d9a1ccf
helmer <helmer.lauronen@gmail.com>
2022-11-28 23:41:07 +0200

    kaksi tuotetta lisää oikein ostoksien määrää

--- viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -58,3 +58,12 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(),"Maito")
         self.assertEqual(ostos.lukumaara(),1)
+
+    def test_kahden_tuotteen_lisaamisen_jalkeen_korissa_kaksi_ostosolio(self):
+        maito = Tuote("Maito", 3)
+        leipa = Tuote("Leipä", 5)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(leipa)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset),2)  
+
