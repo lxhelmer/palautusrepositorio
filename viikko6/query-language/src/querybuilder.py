f7d0e216f9a6dbacce19aa0cb36c34d70b567ecb
helmer <helmer.lauronen@gmail.com>
2022-12-12 21:58:25 +0200

    muutos kyselyksi

---
+++ viikko6/query-language/src/querybuilder.py
@@ -0,0 +1,20 @@
+from matchers import All, PlaysIn, HasAtLeast, HasFewerThan, And, Or
+class QueryBuilder:
+
+    def __init__(self, query = All()):
+        self._query = query
+
+    def playsIn(self, team):
+        return QueryBuilder(And(self._query, PlaysIn(team)))
+
+    def hasAtLeast(self,value, attr):
+        return QueryBuilder(And(self._query, HasAtLeast(value,attr)))
+    
+    def hasFewerThan(self,value,attr):
+        return QueryBuilder(And(self._query, HasFewerThan(value, attr)))
+
+    def oneOf(self, *matchers):
+        return QueryBuilder(And(self._query, Or(*matchers)))
+
+    def build(self):
+        return self._query
